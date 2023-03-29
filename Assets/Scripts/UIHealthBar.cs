b5e1be60d281a779fff6fea360404ce42afb9f4e
Sons123 <100420581+Sons123@users.noreply.github.com>
2023-03-29 10:58:10 +0100

    Scripting the Health Bar

---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHealthBar : MonoBehaviour
+{
+
+    public UnityEngine.UI.Image mask;
+    float originalSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalSize = mask.rectTransform.rect.width;
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
